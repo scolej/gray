4fc50137fad09e2912f747d5927a669a604098b8
sjc <s.cole.j@gmail.com>
2020-04-01 14:34:31 +1100

    inside experiment

--- ray.scm
+++ ray.scm
@@ -38,6 +38,9 @@ range C through D."
 (define (blend as bs x)
   (map (lambda (a b) (lerp a b x)) as bs))
 
+(define (attenuate z xs)
+  (map (lambda (x) (lerp 0 x z)) xs))
+
 (define (clip min max x)
   (cond ((> x max) max)
         ((< x min) min)
@@ -48,6 +51,12 @@ range C through D."
     (list (rescale -1 1 0.5 0.9 v)
           (rescale -1 1 0.5 0.9 v)
           (rescale -1 1 0.9 1.0 v))))
+
+(define (reflect ray pos normal)
+  "Make a new ray, reflected perfectly by NORMAL at POS."
+  (let* ((d0 (ray-dir ray))
+         (d1 (v+ d0 (v* normal (* -2 (vdot normal d0))))))
+    (make-ray pos d1 (1+ (ray-bounces ray)))))
 
 (define (hit-sphere centre radius ray)
   "Determine if RAY hits a sphere at CENTRE with RADIUS.
@@ -65,61 +74,72 @@ get to the first intersection with the sphere's surface."
 
 (define (sphere centre radius)
   (lambda (ray world)
-    (let ((hit (hit-sphere centre radius ray)))
-      (if (not hit) #f
-          (cons
-           hit
-           (lambda ()
-             (let* ((hit-point (ray-at ray hit))
-                    (normal (vunit (v- hit-point centre))))
-               (blend '(1 0 0)
-                      (trace world
-                             (make-ray hit-point
-                                       normal
-                                       (1+ (ray-bounces ray))))
-                      0.9))))))))
+    (if #f ;; (< (vnorm (v- (ray-origin ray) centre)) radius)
+        'inside
+        (let ((t (hit-sphere centre radius ray)))
+          (if (or (not t) (< t 0))
+              'missed
+              (cons
+               t
+               (lambda ()
+                 (let* ((hit-point (ray-at ray t))
+                        (normal (vunit (v- hit-point centre))))
+                   (blend '(1 0 0)
+                          (trace world (reflect ray hit-point normal))
+                          0.5)))))))))
 
 (define (inf-plane y)
   "Infinite horizontal green plane offset vertically by Y."
   (let ((n (make-vec3 0 1 0))
         (po (make-vec3 0 y 0)))
     (lambda (ray world)
-      (let ((denom (vdot n (ray-dir ray))))
-        (if (>= denom 0) #f
-            (let* ((po->o (v- (ray-origin ray) po))
-                   (t (/ (vdot n po->o) denom -1))
-                   (hit-point (ray-at ray t)))
-              (cons t
-                    (lambda ()
-                      (blend
-                       ;; Blend towards sky as we get further away
-                       (blend '(0.3 0.6 0.3) '(0.7 0.8 0.9)
-                              (- 1 (/ 1 (+ (* 0.3 t) 1))))
-                       (trace world
-                              (make-ray hit-point
-                                        (make-vec3 0 1 0)
-                                        (1+ (ray-bounces ray))))
-                       0.5)
-                      ))))))))
+      (if #f ;; (< (vec3-y (ray-origin ray)) y)
+          'inside
+          (let ((denom (vdot n (ray-dir ray))))
+            (if (>= denom 0)
+                'missed
+                (let* ((po->o (v- (ray-origin ray) po))
+                       (t (/ (vdot n po->o) denom -1))
+                       (hit-point (ray-at ray t)))
+                  (if (< t 0)
+                      'missed
+                      (cons
+                       t
+                       (lambda ()
+                         (blend
+                          ;; Blend towards sky as we get further away
+                          (blend '(0.3 0.6 0.3) '(0.7 0.8 0.9) (- 1 (/ 1 (+ (* 0.3 t) 1))))
+                          (trace world (reflect ray hit-point n))
+                          0.5)))))))))))
 
 (define world
-  (list (sphere (make-vec3 -1 0 -2.3) 0.7)
-        (sphere (make-vec3 0 0 -2.0) 0.7)
-        (sphere (make-vec3 1 0 -1.8) 0.7)
-        (inf-plane -0.5)))
+  (list (sphere (make-vec3 -3 2 -5.3)
+                0.7)
+        (sphere (make-vec3 0 0 -3.0)
+                0.7)
+        (sphere (make-vec3 0.7 -0.8 -1.8)
+                0.7)
+        (inf-plane -0.8)))
 
 (define (trace world ray)
   "Each entry in WORLD represents an object. The entry should be a
-function of RAY and WORLD which returns either #f to indicate the ray
-does not hit the object or a pair (T . F). T is the distance travelled
-along RAY before hitting the object. F is a function of no arguments
-which determines the colour seen along RAY."
-  (let ((hs (sort (remove not (map (lambda (o) (o ray world)) world))
-                  (lambda (a b) (< (car a) (car b))))))
-    (if (or (null? hs)
-            (> (ray-bounces ray) 5))
-        (bg ray)
-        ((cdr (car hs))))))
+function of RAY and WORLD which returns one of:
+
+ - The symbol 'missed to indicate the ray does not hit the object.
+
+ - The symbol 'inside to indicate that the ray originates inside the object
+
+ - A pair (T . F) where T is the distance travelled along RAY before
+hitting the object. F is a function of no arguments which determines
+the colour seen along RAY."
+  (let ((objects (map (lambda (o) (o ray world)) world)))
+    (cond
+     ((> (ray-bounces ray) 10) '(0 0 0))
+     ((any (lambda (s) (eq? s 'inside)) objects) '(0 0 0))
+     ((every (lambda (s) (eq? s 'missed)) objects) (bg ray))
+     (else ((cdr (car (sort (remove (lambda (v) (not (pair? v)))
+                                    objects)
+                            (lambda (a b) (< (car a) (car b)))))))))))
 
 (define (write-ppm s w h f)
   (display "P3" s) (newline s)
@@ -127,8 +147,9 @@ which determines the colour seen along RAY."
   (display "255" s) (newline s)
   (let ((aspect (/ w h)))
     (do ((y 0 (1+ y))) ((= y h))
+      ;; Progress indicator
+      (format #t "~d " (inexact->exact (round (/ y h 0.01))))
       (do ((x 0 (1+ x))) ((= x w))
-        (format #t "~d ~d\n" x y)
         (display
          (string-join
           (map
@@ -153,19 +174,18 @@ which determines the colour seen along RAY."
           " ") s)
         ;; FIXME trail space
         (display " " s))
-      (newline s))))
+      (newline s)))
+  (format #t "Done!\n"))
 
 (define (img-fun x y)
   (trace world (make-ray
                 (make-vec3 0 0 0)
-                (make-vec3 x y -1)
+                (vunit (make-vec3 x y -1.1))
                 0)))
 
 
 (call-with-output-file "img.ppm"
   (lambda (s)
-    (write-ppm s  100 80 img-fun)))
-
-;;
-;;
-;;
+    (write-ppm s 70 50 img-fun)
+    ;; (write-ppm s  640 480 img-fun)
+    ))
