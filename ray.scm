919461d3fd91171f4a668d6125d384fbebd32cf1
sjc <s.cole.j@gmail.com>
2020-04-01 12:05:50 +1100

    shadows working! bouncing wrong

--- ray.scm
+++ ray.scm
@@ -5,10 +5,11 @@
  (vector))
 
 (define-record-type ray
-  (make-ray origin direction)
+  (make-ray origin direction bounces)
   ray?
   (origin ray-origin)
-  (direction ray-dir))
+  (direction ray-dir)
+  (bounces ray-bounces))
 
 (define (ray-at r t)
   (v+ (ray-origin r)
@@ -71,8 +72,12 @@ get to the first intersection with the sphere's surface."
            (lambda ()
              (let* ((hit-point (ray-at ray hit))
                     (normal (vunit (v- hit-point centre))))
-               (map (lambda (x) (clip 0 1 x))
-                    (list 1 (vec3-y normal) (vec3-z normal))))))))))
+               (blend '(1 0 0)
+                      (trace world
+                             (make-ray hit-point
+                                       normal
+                                       (1+ (ray-bounces ray))))
+                      0.9))))))))
 
 (define (inf-plane y)
   "Infinite horizontal green plane offset vertically by Y."
@@ -82,12 +87,20 @@ get to the first intersection with the sphere's surface."
       (let ((denom (vdot n (ray-dir ray))))
         (if (>= denom 0) #f
             (let* ((po->o (v- (ray-origin ray) po))
-                   (t (/ (vdot n po->o) denom -1)))
+                   (t (/ (vdot n po->o) denom -1))
+                   (hit-point (ray-at ray t)))
               (cons t
                     (lambda ()
-                      ;; Blend towards sky as we get further away
-                      (blend '(0.3 0.6 0.3) '(0.7 0.8 0.9)
-                             (- 1 (/ 1 (+ (* 0.3 t) 1))))))))))))
+                      (blend
+                       ;; Blend towards sky as we get further away
+                       (blend '(0.3 0.6 0.3) '(0.7 0.8 0.9)
+                              (- 1 (/ 1 (+ (* 0.3 t) 1))))
+                       (trace world
+                              (make-ray hit-point
+                                        (make-vec3 0 1 0)
+                                        (1+ (ray-bounces ray))))
+                       0.5)
+                      ))))))))
 
 (define world
   (list (sphere (make-vec3 -1 0 -2.3) 0.7)
@@ -95,7 +108,7 @@ get to the first intersection with the sphere's surface."
         (sphere (make-vec3 1 0 -1.8) 0.7)
         (inf-plane -0.5)))
 
-(define (hit world ray)
+(define (trace world ray)
   "Each entry in WORLD represents an object. The entry should be a
 function of RAY and WORLD which returns either #f to indicate the ray
 does not hit the object or a pair (T . F). T is the distance travelled
@@ -103,7 +116,8 @@ along RAY before hitting the object. F is a function of no arguments
 which determines the colour seen along RAY."
   (let ((hs (sort (remove not (map (lambda (o) (o ray world)) world))
                   (lambda (a b) (< (car a) (car b))))))
-    (if (null? hs)
+    (if (or (null? hs)
+            (> (ray-bounces ray) 5))
         (bg ray)
         ((cdr (car hs))))))
 
@@ -114,6 +128,7 @@ which determines the colour seen along RAY."
   (let ((aspect (/ w h)))
     (do ((y 0 (1+ y))) ((= y h))
       (do ((x 0 (1+ x))) ((= x w))
+        (format #t "~d ~d\n" x y)
         (display
          (string-join
           (map
@@ -141,8 +156,10 @@ which determines the colour seen along RAY."
       (newline s))))
 
 (define (img-fun x y)
-  (hit world (make-ray (make-vec3 0 0 0)
-                       (make-vec3 x y -1))))
+  (trace world (make-ray
+                (make-vec3 0 0 0)
+                (make-vec3 x y -1)
+                0)))
 
 
 (call-with-output-file "img.ppm"
